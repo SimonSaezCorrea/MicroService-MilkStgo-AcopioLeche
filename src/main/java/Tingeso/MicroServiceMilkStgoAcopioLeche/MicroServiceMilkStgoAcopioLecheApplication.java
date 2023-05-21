ab35e5d04e94edc893446f64bb92078fdaaf5709
Simon Saez <nayxxaxx789super@gmail.com>
2023-05-21 19:08:56 -0400

    Creación de Proyecto IntelliJ

---
+++ src/main/java/Tingeso/MicroServiceMilkStgoAcopioLeche/MicroServiceMilkStgoAcopioLecheApplication.java
@@ -0,0 +1,13 @@
+package Tingeso.MicroServiceMilkStgoAcopioLeche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceMilkStgoAcopioLecheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceMilkStgoAcopioLecheApplication.class, args);
+	}
+
+}
